7a0bd54015b3d0d89ac48130bcc082af722a4404
Patrick Tunezerwane <tunezepatrick@gmail.com>
2021-11-24 10:35:11 +0200

    done

---
+++ c1/main.js
@@ -0,0 +1,10 @@
+console.log('this is the test');
+var a = 8999;
+console.log('this is the variable testing', a);
+var str = 'this is a string';
+// str =  5 // imposible
+str = 'an other string';
+var calc = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log('THIS IS THE ANS', calc(50));
---
+++ c1/main.ts
@@ -0,0 +1,15 @@
+console.log('this is the test')
+
+const a = 8999
+
+console.log('this is the variable testing', a)
+
+let str = 'this is a string'
+// str =  5 // imposible
+str = 'an other string'
+
+const calc = (diameter: number) => {
+    return diameter * Math.PI
+}
+
+console.log('THIS IS THE ANS', calc(50))
